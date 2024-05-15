f5363da20682ce8dfd62e2d9145acae19557b1a0
Kiko Gonzalez Amarillo <166316076+KikolasDEV@users.noreply.github.com>
2024-05-15 12:03:11 +0200

    prueba

--- widgets_basicos/android/app/src/main/kotlin/com/example/widgets_basicos/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.widgets_basicos
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- widgets_basicos/lib/models/Favoritos.dart
+++
@@ -1,7 +0,0 @@
-class Favorito {
-  String nombre;
-  String imagen;
-  int precio;
-
-  Favorito(this.nombre, this.imagen, this.precio);
-}
